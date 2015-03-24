5a40933eb9ff55c05e5a1f5f716393816f755d5e
filips92 <filsmu@wp.pl>
2015-03-24 12:49:17 +0100

    Very basic users form; refreshed users index view

--- scripts/controllers/users/indexController.js
+++ scripts/controllers/users/indexController.js
@@ -2,4 +2,11 @@ app.controller('UsersIndexController', ['$scope', 'Auth', function ($scope, Auth
   Auth.redirectIfNotAuthorized();
 
   $scope.message = 'Hello Bootstrap from UsersIndexController!';
+  $scope.users = [{firstName:'Lesley',lastName:'Tristan'},
+                  {firstName:'Vergil', lastName:'Gareth'},
+                  {firstName:'Dannie', lastName:'Wallace'},
+                  {firstName:'Layton', lastName:'Liam'},
+                  {firstName:'Tom', lastName:'Leroy'},
+                  {firstName:'Kirby', lastName:'Ellery'}];
+     
 }]);
