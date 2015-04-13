eb181c6b00c8aeaf06df722fade8014134be35ea
filips92 <filsmu@wp.pl>
2015-04-13 20:23:08 +0200

    View for adding new insurance policy

--- scripts/controllers/insurances/createOrEditController.js
+++ scripts/controllers/insurances/createOrEditController.js
@@ -1,5 +1,12 @@
 app.controller('InsurancesCreateOrEditController', ['$scope', 'Auth', function ($scope, Auth) {
   Auth.redirectIfNotAuthorized();
 
-  $scope.message = 'Hello Bootstrap from InsurancesCreateOrEditController!';
+  $scope.message = 'New insurance policy';
+    
+  $scope.users = [{id:'1',first_name:'Lesley',last_name:'Tristan',created_at:'2015-03-24',updated_at:'2015-03-24'},
+                  {id:'2',first_name:'Vergil', last_name:'Gareth',created_at:'2015-03-24',updated_at:'2015-03-24'},
+                  {id:'3',first_name:'Dannie', last_name:'Wallace',created_at:'2015-03-24',updated_at:'2015-03-24'},
+                  {id:'4',first_name:'Layton', last_name:'Liam',created_at:'2015-03-24',updated_at:'2015-03-24'},
+                  {id:'5',first_name:'Tom', last_name:'Leroy',created_at:'2015-03-24',updated_at:'2015-03-24'},
+                  {id:'6',first_name:'Kirby', last_name:'Ellery',created_at:'2015-03-24',updated_at:'2015-03-24'}];
 }]);
