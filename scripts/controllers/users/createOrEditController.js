ac0e4857fd0a6025a9983681eeb95f95c53213f8
filips92 <filsmu@wp.pl>
2015-05-19 00:54:46 +0200

    Adding new users

--- scripts/controllers/users/createOrEditController.js
+++ scripts/controllers/users/createOrEditController.js
@@ -1,5 +1,30 @@
-app.controller('UsersCreateOrEditController', ['$scope', 'Auth', function ($scope, Auth) {
+app.controller('UsersCreateOrEditController', ['$scope', 'Auth', 'SimpleHttp', function ($scope, Auth, SimpleHttp) {
   Auth.redirectIfNotAuthorized();
 
   $scope.message = 'New user';
+    
+        $scope.responseData = [];
+    var headers = {
+        'X-User-Email': Auth.credentials.email,
+        'X-User-Token': Auth.credentials.authToken
+    };
+    
+    $scope.form = {
+        first_name : '',
+        last_name : ''
+    };
+    
+    $scope.formSubmitCreateUser = function(){
+         SimpleHttp.headerRequest('post', 'http://iap1.null.yt/api/v1/customers', $scope.form, headers)
+            .then(function(response) {
+              if(response.status === 200 &&
+                 response.data.success) {
+                  $scope.responseData = response.data.data;                     
+              }      
+              else {
+                Auth.handleError(response.data.error);
+              } 
+         });
+        location.replace('#/users');
+    };
 }]);
